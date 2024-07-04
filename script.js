73f5b4c1459acbc8ad6cf2fe26764bcbdc905924
Aamir Hussain <aamirbhat382@gmail.com>
2024-07-04 16:55:22 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript for Falah Innovations
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Falah Innovations website loaded');
+    // Add any interactive features here
+  });
+  
